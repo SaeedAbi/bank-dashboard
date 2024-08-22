c8b550d306dea3e5e41a4b5e0bc25fa01feebc51
SaeedAbi <abdary.saeed@gmail.com>
2024-08-22 19:55:55 +0330

    🚧 add aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,90 @@
+'use client';
+
 import React from 'react';
+import { useWindowSize } from '@/hooks/helpers';
+import Link from 'next/link';
+import {
+  FaCircleDollarToSlot,
+  FaCoins,
+  FaCreditCard,
+  FaHandHoldingDollar,
+  FaHouse,
+  FaMoneyBillTransfer,
+  FaRegUser,
+  FaToolbox,
+} from 'react-icons/fa6';
+import { IoSettingsSharp } from 'react-icons/io5';
+import { Image } from 'antd';
 
 function Aside() {
-  return (
+  const { isMobile } = useWindowSize();
+
+  return isMobile ? null : (
     <aside className='w-[230px] flex-shrink-0 flex-grow-0 bg-blue-800'>
-      aside goes here
+      <Image
+        src='/../../../public/Logo.png'
+        alt='logo image'
+        width={183}
+        height={36}
+      />
+      <nav>
+        <ul className='flex flex-col'>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaHouse />
+              <span>Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaMoneyBillTransfer />
+              <span>Transactions</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaRegUser />
+              <span>Accounts</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaCoins />
+              <span>Investments</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaCreditCard />
+              <span>Credit Cards</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaHandHoldingDollar />
+              <span>Loans</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaToolbox />
+              <span>Services</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <FaCircleDollarToSlot />
+              <span>My Privileges</span>
+            </Link>
+          </li>
+          <li>
+            <Link className='flex items-center' href='#'>
+              <IoSettingsSharp />
+              <span>Setting</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </aside>
   );
 }
