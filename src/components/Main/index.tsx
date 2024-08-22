f3ee8547b17abc0483dbc26b0651a366260546b3
SaeedAbi <abdary.saeed@gmail.com>
2024-08-22 19:56:13 +0330

    🚧 add main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import React from 'react';
 
 function Main() {
-  return <main className='flex-shrink flex-grow bg-green-600 md:bg-purple-700'></main>;
+  return (
+    <main className='flex-shrink flex-grow bg-green-600 md:bg-purple-700'></main>
+  );
 }
 
 export default Main;
