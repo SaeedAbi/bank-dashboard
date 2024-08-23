70fdc8cb2de20844ec7bcb5a5bfc443db4a947dd
SaeedAbi <abdary.saeed@gmail.com>
2024-08-23 18:16:08 +0330

    🎉 update template

---
+++ src/app/investments/page.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Investment() {
+  return <div>investment</div>;
+}
+
+export default Investment;
---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Transaction() {
+  return <div>Transaction</div>;
+}
+
+export default Transaction;
