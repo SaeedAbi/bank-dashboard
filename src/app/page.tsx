3f492cb548844b56f2b83a8db2e33faaa758c23b
SaeedAbi <abdary.saeed@gmail.com>
2024-08-24 15:02:15 +0330

    🚧 add myCards component to overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import MyCards from '@/components/Main/Overview/MyCards';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      sss
+    <main className='h-[923px] max-w-[1190px] overflow-auto bg-mainBg'>
+      <section className='mx-[40px]'>
+        <MyCards/>
+      </section>
     </main>
   );
 }
