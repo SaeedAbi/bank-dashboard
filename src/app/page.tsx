ebeb700fb58ea02421745226b4d64e3af851a13b
SaeedAbi <abdary.saeed@gmail.com>
2024-08-24 18:30:58 +0330

    🚧 add recentTransaction component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import MyCards from '@/components/Main/Overview/MyCards';
+import RecentTransactions from '@/components/Main/Overview/RecentTransactions';
 
 export default function Home() {
   return (
     <main className='h-[923px] max-w-[1190px] overflow-auto bg-mainBg'>
       <section className='mx-[40px]'>
-        <MyCards/>
+        <div className='flex gap-[30px]'>
+          <MyCards />
+          <RecentTransactions />
+        </div>
       </section>
     </main>
   );
