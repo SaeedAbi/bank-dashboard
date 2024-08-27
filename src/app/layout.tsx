de241f4cedaa3120eab6d5a9a210ced033d47854
SaeedAbi <abdary.saeed@gmail.com>
2024-08-27 18:41:34 +0330

    🚧 add initial loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,31 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
+        <div id={'initial-loading'}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            preserveAspectRatio='xMidYMid'
+            viewBox='0 0 100 100'
+          >
+            <g>
+              <path
+                fill='none'
+                stroke='#e15b64'
+                strokeWidth='12'
+                d='M50 15a35 35 0 1024.749 10.251'
+              />
+              <path fill='#e15b64' d='M49 3v24l12-12L49 3' />
+              <animateTransform
+                attributeName='transform'
+                dur='1s'
+                keyTimes='0;1'
+                repeatCount='indefinite'
+                type='rotate'
+                values='0 50 50;360 50 50'
+              />
+            </g>
+          </svg>
+        </div>
         <AntdRegistry>
           <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} />
---
+++ src/utils/loading.ts
@@ -0,0 +1,25 @@
+// by removing `hide` class
+export function registerLoading() {
+  const loadingElement = document.getElementById('initial-loading');
+
+  if (loadingElement) {
+    const classes = loadingElement.classList;
+
+    if (classes.contains('hide')) {
+      classes.remove('hide');
+    }
+  }
+}
+
+// by adding `hide` class
+export function hideLoading() {
+  const loadingElement = document.getElementById('initial-loading');
+
+  if (loadingElement) {
+    const classes = loadingElement.classList;
+
+    if (!classes.contains('hide')) {
+      classes.add('hide');
+    }
+  }
+}
