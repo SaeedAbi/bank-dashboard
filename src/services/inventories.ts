0d8a5c175bfaf76b9c376352c3effa82b74d036c
SaeedAbi <abdary.saeed@gmail.com>
2024-08-27 18:42:35 +0330

    🚧 add random delay in order to simulate network latency

--- src/services/inventories.ts
+++ src/services/inventories.ts
@@ -16,28 +16,37 @@ import {
   services,
   stocks,
 } from '@/db';
+import { randomDelayForAPIServices, sleep } from '@/utils/helper';
 
 export async function getAllCurrencies(): Promise<CurrencyType[]> {
+  await randomDelayForAPIServices();
   return currencies;
 }
 export async function getAllPositions(): Promise<InventoryType[]> {
+  await randomDelayForAPIServices();
   return positions;
 }
 export async function getAllCategories(): Promise<InventoryType[]> {
+  await randomDelayForAPIServices();
   return categories;
 }
 export async function getAllBanks(): Promise<BankType[]> {
+  await randomDelayForAPIServices();
   return banks;
 }
 export async function getAllCreditCards(): Promise<InventoryType[]> {
+  await randomDelayForAPIServices();
   return creditCards;
 }
 export async function getAllServices(): Promise<ServiceType[]> {
+  await randomDelayForAPIServices();
   return services;
 }
 export async function getAllLoans(): Promise<LoanType[]> {
+  await randomDelayForAPIServices();
   return loans;
 }
 export async function getAllStocks(): Promise<StokeType[]> {
+  await randomDelayForAPIServices();
   return stocks;
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -9,3 +9,15 @@ export function debounce(fn: (...args: unknown[]) => void, delay: number) {
 export function isServer(): boolean {
   return typeof window === 'undefined';
 }
+
+// amount in `ms`
+export async function sleep(delay: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+export async function randomDelayForAPIServices() {
+  const randomNumber = Math.random();
+  return sleep(randomNumber * 2 + 1);
+}
