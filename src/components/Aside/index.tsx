150aa56a653ffdea917ef4bd3e70ba88e86a37b6
SaeedAbi <abdary.saeed@gmail.com>
2024-08-28 17:19:07 +0330

    ♻️ add a map function to sidebar and change style of button

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -19,6 +19,49 @@ import logo from '../../../public/Logo.png';
 import styles from './styles.module.scss';
 import { PRIVATE_ROUTES } from '@/routes';
 
+const links = [
+  {
+    label: 'Dashboard',
+    icon: <FaHouse />,
+    href: PRIVATE_ROUTES.dashboard,
+  },
+  {
+    label: 'Transactions',
+    icon: <FaMoneyBillTransfer />,
+    href: PRIVATE_ROUTES.transactions,
+  },
+  {
+    label: 'Accounts',
+    icon: <FaRegUser />,
+    href: PRIVATE_ROUTES.accounts,
+  },
+  {
+    label: 'Investments',
+    icon: <FaCoins />,
+    href: PRIVATE_ROUTES.investments,
+  },
+  {
+    label: 'Credit Cards',
+    icon: <FaCreditCard />,
+    href: PRIVATE_ROUTES.cards,
+  },
+  {
+    label: 'Loans',
+    icon: <FaHandHoldingDollar />,
+    href: PRIVATE_ROUTES.loans,
+  },
+  {
+    label: 'Services',
+    icon: <FaToolbox />,
+    href: PRIVATE_ROUTES.services,
+  },
+  {
+    label: 'Setting',
+    icon: <IoSettingsSharp />,
+    href: PRIVATE_ROUTES.settings,
+  },
+];
+
 function Aside() {
   const { isMobile } = useWindowSize();
 
@@ -28,79 +71,20 @@ function Aside() {
         <Image src={logo} alt='logo image' width={183} height={36} />
       </div>
       <nav className={styles.secondRow}>
-        <ul className='flex flex-col gap-9'>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.dashboard}
-            >
-              <FaHouse />
-              <span>Dashboard</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.transactions}
-            >
-              <FaMoneyBillTransfer />
-              <span>Transactions</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.accounts}
-            >
-              <FaRegUser />
-              <span>Accounts</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.investments}
-            >
-              <FaCoins />
-              <span>Investments</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.cards}
-            >
-              <FaCreditCard />
-              <span>Credit Cards</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.loans}
-            >
-              <FaHandHoldingDollar />
-              <span>Loans</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.services}
-            >
-              <FaToolbox />
-              <span>Services</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className='text-lg flex items-center gap-[26px] font-medium text-secondary hover:text-primary'
-              href={PRIVATE_ROUTES.settings}
-            >
-              <IoSettingsSharp height={25} width={25} />
-              <span>Setting</span>
-            </Link>
-          </li>
+        <ul className='flex flex-col'>
+          {links.map((link) => {
+            return (
+              <li key={link.label}>
+                <Link
+                  className='text-lg hover:border-l-darkBlue flex items-center gap-[26px] py-5 pl-11 font-medium text-secondary hover:border-l-[6px] hover:text-primary'
+                  href={link.href}
+                >
+                  {link.icon}
+                  <span>{link.label}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </aside>
