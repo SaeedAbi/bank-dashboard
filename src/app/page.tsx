21725a966540d9e3c80991c9bd32fa62ba329334
SaeedAbi <abdary.saeed@gmail.com>
2024-08-29 16:59:00 +0330

    🚧 add balanceHistory component to overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,24 @@ import RecentTransactions from '@/components/Main/Overview/RecentTransactions';
 import WeeklyActivities from '@/components/Main/Overview/WeeklyActivities';
 import ExpenseStatistics from '@/components/Main/Overview/ExpenseStatistics';
 import QuickTransfer from '@/components/Main/Overview/QuickTransfer';
+import BalanceHistory from '@/components/Main/Overview/BalanceHistory';
 
 export default function Home() {
   return (
     <main className='bg-mainBg'>
       <div className='mx-10 flex flex-wrap overflow-auto'>
-        <MyCards />
-        <RecentTransactions />
-        <WeeklyActivities />
-        <ExpenseStatistics />
-        <QuickTransfer />
+        <div className={'flex gap-[30px] flex-wrap'}>
+          <MyCards />
+          <RecentTransactions />
+        </div>
+        <div className={'flex gap-[30px] flex-wrap'}>
+          <WeeklyActivities />
+          <ExpenseStatistics />
+        </div>
+        <div className={'flex gap-[30px] flex-wrap'}>
+          <QuickTransfer />
+          <BalanceHistory />
+        </div>
       </div>
     </main>
   );
--- src/components/Main/Overview/RecentTransactions.tsx
+++ src/components/Main/Overview/RecentTransactions.tsx
@@ -7,7 +7,7 @@ function RecentTransactions() {
       <div className='text-3xl font-semibold text-primary'>
         Recent Transactions
       </div>
-      <div className='mt-5 flex h-[235px] w-[350px] flex-shrink-0 flex-col justify-center rounded-[25px] bg-white pb-[30px]'>
+      <div className='mt-5 flex h-[235px] w-[350px] flex-col justify-center rounded-[25px] bg-white pb-[30px]'>
         <LastTransactionItem />
         <LastTransactionItem />
         <LastTransactionItem />
