db3572cafbdb65a30ce3924b2ad9f21de70ec9ea
SaeedAbi <abdary.saeed@gmail.com>
2024-08-29 14:14:06 +0330

    🚧 add QuickTransfer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,17 @@ import MyCards from '@/components/Main/Overview/MyCards';
 import RecentTransactions from '@/components/Main/Overview/RecentTransactions';
 import WeeklyActivities from '@/components/Main/Overview/WeeklyActivities';
 import ExpenseStatistics from '@/components/Main/Overview/ExpenseStatistics';
+import QuickTransfer from '@/components/Main/Overview/QuickTransfer';
 
 export default function Home() {
   return (
-    <main className='mx-10'>
-      <div className='flex flex-wrap gap-[30px] overflow-auto'>
+    <main className='bg-mainBg'>
+      <div className='mx-10 flex flex-wrap overflow-auto'>
         <MyCards />
         <RecentTransactions />
         <WeeklyActivities />
         <ExpenseStatistics />
+        <QuickTransfer />
       </div>
     </main>
   );
