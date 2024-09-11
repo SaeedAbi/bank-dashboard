10b27f611d4a4507d59375124cdca965c4337309
SaeedAbi <abdary.saeed@gmail.com>
2024-09-11 19:28:32 +0330

    🚧 add query hooks for users and transactions

--- src/hooks/queries.ts
+++ src/hooks/queries.ts
@@ -2,10 +2,12 @@
 // Keys
 //=============================================
 import { useQuery } from "@tanstack/react-query";
-import { CardType } from "@/interfaces";
+import { CardType, TransactionType, UserType } from "@/interfaces";
 import { fetchAPI } from "@/utils/helper";
 
 export const CARD_KEY = "CARD_KEY";
+export const USER_KEY = "USER_KEY";
+export const TRANSACTION_KEY = "TRANSACTION_KEY";
 
 //=============================================
 // Queries
@@ -16,5 +18,26 @@ export function useCards() {
     async queryFn() {
       return fetchAPI<CardType[]>("/cards");
     },
+    suspense: true,
+  });
+}
+
+export function useUsers() {
+  return useQuery({
+    queryKey: [USER_KEY],
+    async queryFn() {
+      return fetchAPI<UserType[]>("/users");
+    },
+    suspense: true,
+  });
+}
+
+export function useTransactions() {
+  return useQuery({
+    queryKey: [TRANSACTION_KEY],
+    async queryFn() {
+      return fetchAPI<TransactionType[]>("/transactions");
+    },
+    suspense: true,
   });
 }
