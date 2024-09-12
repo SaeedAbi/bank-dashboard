709730a0cca3339e149d437152b8f50563d7adb2
SaeedAbi <abdary.saeed@gmail.com>
2024-09-12 18:43:00 +0330

    💄 change loading style

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loading() {
   return (
     <div className={"flex flex-col w-full"}>
-      <div className={"flex flex-nowrap gap-x-3"}>
+      <div className={"flex flex-nowrap gap-x-3 h-1/3 pt-3"}>
         <div role="status" className="w-1/3 animate-pulse">
           <div className="h-2.5  bg-gray-400 rounded-full w-48 mb-4" />
           <div className="h-2  bg-gray-400 rounded-full mb-2.5" />
@@ -32,7 +32,7 @@ function Loading() {
           <span className="sr-only">Loading...</span>
         </div>
       </div>
-      <div className={"flex flex-nowrap gap-x-5"}>
+      <div className={"flex flex-nowrap gap-x-5 h-2/3"}>
         <div role="status" className="w-1/2 animate-pulse">
           <div className="flex items-baseline mt-4">
             <div className="w-full  bg-gray-400 rounded-t-lg h-72 "></div>
