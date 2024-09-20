485b6bbb13be269eddd17a94ed5045db4853424f
SaeedAbi <abdary.saeed@gmail.com>
2024-09-20 15:40:38 +0330

    🚧 add My expense column

---
+++ src/app/transactions/_MyExpense.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Column } from "@ant-design/plots";
+
+const expense = [
+  { date: "A", amount: 2200 },
+  { date: "B", amount: 1600 },
+  { date: "C", amount: 1700 },
+  { date: "D", amount: 20 },
+  { date: "E", amount: 900 },
+  { date: "F", amount: 690 },
+  { date: "G", amount: 0 },
+];
+
+function _MyExpense() {
+  const config = {
+    data: {
+      value: expense,
+    },
+    xField: "date",
+    yField: "amount",
+    label: {
+      textBaseline: "bottom",
+    },
+    xAxis: {
+      line: { style: { lineWidth: 0 } },
+      tickLine: { style: { display: "none" } },
+      label: "none",
+    },
+    yAxis: {
+      label: null, // Hides the Y-axis labels
+      line: null, // Hides the Y-axis line
+
+      grid: { line: { style: { lineWidth: 0 } } },
+    },
+    columnStyle: {
+      fillOpacity: 0.7,
+    },
+    style: {
+      radius: 7,
+    },
+  };
+  return (
+    <div className="overflow-auto">
+      <div className="mb-[20px] mt-[25px] text-3xl font-semibold text-primary">
+        My Expense
+      </div>
+      <Column
+        className={"bg-white rounded-[25px]"}
+        width={350}
+        height={245}
+        {...config}
+      />
+    </div>
+  );
+}
+
+export default _MyExpense;
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import * as React from "react";
+import MyCards from "@/components/Main/Overview/MyCards";
+import { useCards, useUsers } from "@/hooks/queries";
+import _MyExpense from "@/app/transactions/_MyExpense";
 
 function Transaction() {
-  return <div>Transaction</div>;
+  const { data: cardData } = useCards();
+  const { data: userData } = useUsers();
+  return (
+    <div>
+      <div className={"flex flex-wrap gap-8"}>
+        <MyCards cards={cardData} users={userData} />
+        <_MyExpense />
+      </div>
+    </div>
+  );
 }
 
 export default Transaction;
