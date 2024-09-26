00be354001e2b937dd49b2de8c4db48a993d1ca2
SaeedAbi <abdary.saeed@gmail.com>
2024-09-26 20:10:24 +0330

    🚧 add pagination and improve the render

--- src/app/transactions/_RecentTransactions.tsx
+++ src/app/transactions/_RecentTransactions.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import { InventoryType, TransactionType } from "@/interfaces";
+import { DownOutlined, UpOutlined } from "@ant-design/icons";
+import { Button, Pagination, PaginationProps } from "antd";
+
 import { currentUserId } from "@/components/Autentication";
+import { ActionType, InventoryType, TransactionType } from "@/interfaces";
 import { formatToddMMMhh } from "@/utils/date";
 
 interface PropTypes {
@@ -9,11 +12,46 @@ interface PropTypes {
   categories?: InventoryType[];
 }
 
-function RecentTransactions({ transactions = [], categories = [] }: PropTypes) {
-  const myTransactions = transactions?.filter(
-    (transaction) => (transaction.userId = currentUserId)
-  );
+interface ExtendedTransactionType extends TransactionType {
+  categoryName?: string;
+}
 
+const PAGE_SIZE = 5;
+
+function RecentTransactions({ transactions = [], categories = [] }: PropTypes) {
+  const [currentIndex, setCurrentIndex] = React.useState(1);
+  const myTransactions = React.useMemo(()=>{
+    return transactions?.filter(
+      (transaction) => transaction.userId === currentUserId
+    )
+  },[transactions]);
+
+  const filteredTransactions = myTransactions.reduce<ExtendedTransactionType[]>(
+    (prev, curr, index) => {
+      const dataCategory = categories?.find(
+        (category) => category.id === curr.category
+      );
+      const lowerLimit = (currentIndex - 1) * PAGE_SIZE;
+      const higherLimit = lowerLimit + PAGE_SIZE - 1;
+
+      if (index >= lowerLimit && index <= higherLimit) {
+        return [
+          ...prev,
+          {
+            ...curr,
+            categoryName: dataCategory ? dataCategory.label : "-",
+          },
+        ];
+      }
+      return prev;
+    },
+    []
+  );
+  console.log(myTransactions);
+
+  const onChange: PaginationProps["onChange"] = (page) => {
+    setCurrentIndex(page);
+  };
   return (
     <div className={"flex flex-col flex-shrink flex-grow"}>
       <div className="mb-7 mt-[25px] text-3xl font-semibold text-primary">
@@ -25,55 +63,79 @@ function RecentTransactions({ transactions = [], categories = [] }: PropTypes) {
         <button className={styles.link}>Expense</button>
       </div>
       <div className={"pt-6 border-t-2 border-b-borderGrey"}>
-        <table className="w-full border-collapse border-gray-200 bg-white rounded-3xl	">
+        <table className="w-full border-collapse border-gray-200 bg-white rounded-3xl">
           <thead>
-            <tr className={"border-b-borderGrey border-b-2"}>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <tr
+              className={
+                "border-b-borderGrey border-b-2 text-textBlue text-base"
+              }
+            >
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Description
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Transaction ID
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Category
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Card
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Date
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Amount
               </th>
-              <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th className="py-3 px-6 text-left tracking-wider font-medium">
                 Receipt
               </th>
             </tr>
           </thead>
           <tbody>
-            {myTransactions.map((transaction) => {
-              const dataCategory = categories?.find(
-                (category) => category.id === transaction.category
-              );
-              return (
-                <tr>
-                  <td className="py-4 px-6">{transaction.description}</td>
-                  <td className="py-4 px-6">{transaction.id}</td>
-                  <td className="py-4 px-6">{dataCategory?.label}</td>
-                  <td className="py-4 px-6">
-                    {transaction.destinationCardNumber}
-                  </td>
-                  <td className="py-4 px-6">
-                    {formatToddMMMhh(transaction.date)}
-                  </td>
-                  <td className="py-4 px-6">${transaction.amount}</td>
-                  <td className="py-4 px-6">download</td>
-                </tr>
-              );
-            })}
+            {filteredTransactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  <span className={"pr-1"}>
+                    {transaction.type === ActionType.withdraw ? (
+                      <UpOutlined style={{ color: "red" }} />
+                    ) : (
+                      <DownOutlined style={{ color: "green" }} />
+                    )}
+                  </span>
+                  <span>{transaction.description}</span>
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  {transaction.id}
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  {transaction.categoryName}
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  {transaction.destinationCardNumber}
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  {formatToddMMMhh(transaction.date)}
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  ${transaction.amount}
+                </td>
+                <td className="py-4 px-6 text-black text-base font-normal">
+                  <Button>download</Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
+      </div>
+      <div className={"pt-6 justify-self-end self-end "}>
+        <Pagination
+          current={currentIndex}
+          onChange={onChange}
+          total={myTransactions.length}
+          defaultPageSize={PAGE_SIZE}
+        />
       </div>
     </div>
   );
