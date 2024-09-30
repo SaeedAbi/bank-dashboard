a389a9791ffd68ea5a0c7177f5549b52d000310e
SaeedAbi <abdary.saeed@gmail.com>
2024-09-30 18:16:06 +0330

    🚧 add lastTransactions to accounts

---
+++ src/app/accounts/_LastTransactions.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { DownSquareOutlined, UpSquareOutlined } from "@ant-design/icons";
+import {
+  ActionType,
+  InventoryType,
+  TransactionStatus,
+  TransactionType,
+} from "@/interfaces";
+import { currentUserId } from "@/components/Autentication";
+import { formatDateTOYYYYMMDD } from "@/utils/date";
+
+interface PropType {
+  transactions?: TransactionType[];
+  categories?: InventoryType[];
+}
+
+interface ExtendedTransactionType extends TransactionType {
+  categoryName?: string;
+}
+
+function LastTransactions({ transactions, categories }: PropType) {
+  const myTransactions = transactions?.filter(
+    (transaction) => transaction.userId === currentUserId
+  );
+
+  const filteredTransactions = myTransactions?.reduce<
+    ExtendedTransactionType[]
+  >((prev, curr) => {
+    const dataCategory = categories?.find(
+      (category) => category.id === curr.category
+    );
+    return [
+      ...prev,
+      {
+        ...curr,
+        categoryName: dataCategory ? dataCategory.label : "-",
+      },
+    ];
+  }, []);
+  const threeLastTransaction = filteredTransactions?.slice(0, 3);
+  return (
+    <div>
+      <div className="mb-[20px] mt-[25px] overflow-auto w-2/3">
+        <div className="mb-5 text-3xl font-semibold text-primary">
+          Last Transaction
+        </div>
+        <div className={"flex flex-col gap-6 bg-white rounded-3xl p-6"}>
+          {threeLastTransaction?.map((transaction) => (
+            <div
+              key={transaction.id}
+              className={"flex items-center justify-between"}
+            >
+              {transaction.type === ActionType.withdraw ? (
+                <UpSquareOutlined
+                  style={{
+                    fontSize: "27px",
+                    color: "red",
+                  }}
+                />
+              ) : (
+                <DownSquareOutlined
+                  style={{
+                    fontSize: "27px",
+                    color: "green",
+                  }}
+                />
+              )}
+              <div className={"pl-6 flex flex-col gap-2"}>
+                <div className={"text-base text-black font-medium"}>
+                  {transaction.description}
+                </div>
+                <div className={"text-textBlue text-sm font-normal"}>
+                  {formatDateTOYYYYMMDD(transaction.date)}
+                </div>
+              </div>
+              <div className={"text-textBlue text-base font-normal"}>
+                {transaction.categoryName}
+              </div>
+              <div className={"text-textBlue text-base font-normal"}>
+                {transaction.destinationCardNumber}
+              </div>
+              <div className={"text-textBlue text-base font-normal"}>
+                {transaction.status === TransactionStatus.complete && (
+                  <div>Complete</div>
+                )}
+                {transaction.status === TransactionStatus.pending && (
+                  <div>Pending</div>
+                )}
+                {transaction.status === TransactionStatus.rejected && (
+                  <div>Rejected</div>
+                )}
+              </div>
+              <div className={"text-textBlue text-base font-normal"}>
+                {transaction.amount}$
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LastTransactions;
--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -2,14 +2,22 @@
 
 import * as React from "react";
 import TotalSummery from "@/app/accounts/_TotalSummery";
-import { useCards, useTransactions } from "@/hooks/queries";
+import LastTransactions from "@/app/accounts/_LastTransactions";
+import { useCards, useCategories, useTransactions } from "@/hooks/queries";
 
 function Accounts() {
   const { data: cardsData } = useCards();
   const { data: transactionsData } = useTransactions();
+  const { data: categoriesData } = useCategories();
   return (
     <div className={"w-full"}>
       <TotalSummery cards={cardsData} transactions={transactionsData} />
+      <div>
+        <LastTransactions
+          transactions={transactionsData}
+          categories={categoriesData}
+        />
+      </div>
     </div>
   );
 }
