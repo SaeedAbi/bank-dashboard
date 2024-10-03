385c596d902786ee8750c252df379781c74313a5
SaeedAbi <abdary.saeed@gmail.com>
2024-10-03 14:21:22 +0330

    🚧 add InvestmentSummery component

--- src/app/investments/page.tsx
+++ src/app/investments/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
 import * as React from "react";
+import InvestmentSummery from "@/app/investments/_InvestmentSummery";
+import MonthlyRev from "@/app/investments/_MontlyRev";
+import DailyRev from "@/app/investments/_DailyRev";
+import MyInvestment from "@/app/investments/_MyInvestment";
+import TrendingStock from "@/app/investments/_TrendingStock";
+import { useStocks } from "@/hooks/queries";
 
 function Investment() {
-  return <div>investment</div>;
+  const { data: stocksData } = useStocks();
+  return (
+    <div className={"w-full"}>
+      <InvestmentSummery stocks={stocksData} />
+      <div>
+        <MonthlyRev />
+        <DailyRev />
+      </div>
+      <div>
+        <MyInvestment />
+        <TrendingStock />
+      </div>
+    </div>
+  );
 }
 
 export default Investment;
